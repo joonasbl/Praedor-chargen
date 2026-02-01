1a7657fb46db35b9c4a0208a0912f46c7aac6aa2
Joonas <joonas.blomberg@gmail.com>
2026-02-01 14:59:23 +0200

    Korjattu taito yhteenveto

--- taito_kontti.gd
+++ taito_kontti.gd
@@ -7,13 +7,16 @@ func alusta():
 	for child in children:
 		if child is Taito:
 			var box = HBoxContainer.new()
+			var margin = MarginContainer.new()
+			margin.add_theme_constant_override("margin_left", 20)
 			var tn = Label.new()
 			tn.text = "%s" % [child.nimi]
 			var ta = Label.new()
 			ta.text = "%d" % [child.arvo]
 			ta.size_flags_horizontal = Control.SIZE_EXPAND_FILL
 			ta.horizontal_alignment = HORIZONTAL_ALIGNMENT_RIGHT
-			box.add_child(tn)
+			box.add_child(margin)
+			margin.add_child(tn)
 			box.add_child(ta)
 			add_child(box)
 			
